8cd72cd787b088cc18996f996c218a705ef90bae
Kinkando <TY_Sage@hotmail.com>
2024-12-09 15:32:57 +0700

    chore: custom theme color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,8 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/module/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/core/**/*.{js,ts,jsx,tsx,mdx}",
     content(),
   ],
   theme: {
@@ -13,6 +15,18 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        green: {
+          100: '#D8E9E4',
+          300: '#2B5F44',
+          500: '#243831',
+        },
+        golden: '#C5A365',
+        text: '#191919',
+        grey: {
+          100: '#BBC2C0',
+          300: '#939494',
+        },
+        success: '#49A569',
       },
     },
   },
